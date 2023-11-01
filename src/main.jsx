82b4fcd8d8caf4e8bd1f6900e723b1be587d03b7
Tejaswa <tejaswadayalsri@gmail.com>
2023-11-01 18:48:17 +0530

    Added custom primary and secondary color to ChakraUI

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+    colors:{
+        primary:{
+            lighter:"#30C7B5",
+            neutral:"#039c89",
+            dark:'#008272'
+          },
+          secondary:{
+            lighter:'#FA7B55',
+            neutral:'#DE6B48',
+            dark:'#CD5A38',
+          }
+    }
+})
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
     <App />
     </ChakraProvider>
   </React.StrictMode>,
